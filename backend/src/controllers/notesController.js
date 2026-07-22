c92c29c5dd7e1e836fc82e4026ee4c6f6dc157a7
Gerrard <gerrarddada@gmail.com>
2026-07-23 06:47:36 +0800

    App developing:     - Front end skeletal react stucture     - Built ratelimiter     - Built note controllers for editing notes     - Built middleware     - Bugfixes

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,17 +1,59 @@
-export const getAllNotes = (req, res) => {
-    res.status(200).send("fetched notes")
+import Note from '../models/Note.js'
+
+
+export const getAllNotes = async (req, res) => {
+    try {
+        const notes = await Note.find();
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error('error in getAllNotes controller', error);
+        res.status(500).json({message: 'Internal server error'})
+    }
 };
 
-export const createNote = (req, res) => {
-    res.status(201).json({message:'note created successfully.'})
+export const createNote = async (req, res) => {
+    try{
+        const {title, content} = req.body
+        const note = new Note({title, content});
+
+        const savedNote = await note.save();
+        res.status(201).json({message: 'Note created successfully'});
+    } catch (error) {
+        console.error('error in createNote controller', error)
+        res.status(500).json({message: 'Internal server error'});
+    }
 };
 
-export const updateNote = (req, res) => {
-    res.status(201).json({message:"note updated successfully."
-    })
+export const updateNote = async (req, res) => {
+    try {
+        const {title, content} = req.body;
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id,
+            {title, content},
+            {
+                new:true,
+            }
+        );
+
+        if (!updatedNote) return res.status(404).json({message:'Note not found'});
+        res.status(200).json(updatedNote);
+    
+    } catch(error) {
+        console.error('error in updateNote controller', error);
+        if (error.name === 'CastError') return res.status(400).json({message: 'Invalid note id'});
+        res.status(500).json({message: 'Internal server error'});
+    }
 }
 
-export const deleteNote = (req, res) => {
-    res.status(201).json({message:"note deleted successfully."
-    })
+export const deleteNote = async (req, res) => {
+    try {
+        const deletedNote = await Note.findByIdAndDelete(req.params.id);
+
+        if (!deletedNote) return res.status(404).json({message:'Note not found'});
+        res.status(200).json(deletedNote);
+
+    } catch(error) {
+        console.error('error in deleteNote controller', error);
+        if (error.name === 'CastError') return res.status(400).json({message: 'Invalid note id'});
+        res.status(500).json({message: 'Internal server error'});
+    }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,32 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js'
+import { connectDB } from './config/db.js'
+import rateLimiter from './middleware/rateLimiter.js';
 
 const app = express();
+const PORT = process.env.PORT || 3000
+
+
+// Middleware
+
+// parse json body
+app.use(express.json());
+app.use(rateLimiter);
+
+/*
+// simple logger to demo middleware
+
+app.use((req, res, next) => {
+    console.log(`Request method is ${req.method} & Request URL is ${req.url}`)
+    next();
+});
+
+*/
 
 app.use('/api/notes', notesRoutes)
 
-app.listen(3000, ()=> {
-    console.log('server started on port 3000')
-})
+connectDB().then(() => {
+        app.listen(PORT, ()=> {
+        console.log('server started on port 3000', PORT);
+    });
+});
